80a72e72742cb1a17dcc58579592124913e670c7
apj_aaronliang <ying5504242>
2020-07-31 17:49:05 +0800

    aa

--- json/data.js
+++ json/data.js
@@ -1,5 +1,5 @@
 const data = [
   { name: '无人之岛', artist: '任然', url: 'music/无人之岛.mp3', cover: 'img/wurenzhidao.jpg', lrc: 'lrc/无人之岛.lrc', theme: '#ebd0c2' },
-  { name: '不该', artist: '周杰伦,aMEI', url: 'music/不该.mp3', cover: 'img/bugai.jpg', lrc: 'lrc/.lrc', theme: '#ebd0c2' },
+  { name: '不该', artist: '周杰伦,aMEI', url: 'music/不该.mp3', cover: 'img/bugai.jpg', lrc: 'lrc/不该.lrc', theme: '#ebd0c2' },
   { name: '外滩十八号', artist: '袁成杰,戚薇', url: 'music/外滩十八号.mp3', cover: 'img/waitanshibahao.jpg', lrc: 'lrc/外滩十八号.lrc', theme: '#ebd0c2' }
 ]

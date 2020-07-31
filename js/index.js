e27a183a8fab8ff7919cedc69408f97216598597
apj_aaronliang <ying5504242>
2020-07-31 10:36:52 +0800

    aa

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(function(){
+    const ap = new APlayer({
+        container: document.getElementById('aplayer'),
+        fixed: false, //开启吸底模式listFolded
+        mini: false,
+        autoplay: true,
+        theme: '#FADFA3',
+        loop: 'all',
+        order: 'list',
+        preload: 'auto',
+        volume: 0.7,
+        mutex: true,
+        listFolded: true,
+        listMaxHeight: 90,
+        lrcType: 3,
+        audio: data
+    });
+})
---
+++ json/data.js
@@ -0,0 +1,10 @@
+const data = [
+  {
+    name: 'name1',
+    artist: 'artist1',
+    url: 'music/任然-无人之岛.mp3',
+    cover: 'img/wurenzhidao.jpg',
+    lrc: 'lrc/任然-无人之岛.lrc',
+    theme: '#ebd0c2'
+  }
+]
